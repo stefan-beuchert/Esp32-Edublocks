479e105327e503898da822a45ce9e1e9fb44ee93
best1029 <39964050+best1029@users.noreply.github.com>
2019-06-01 11:36:14 +0200

    Update definitions.ts

--- edublocks-micropython/src/blocks/infrared/definitions.ts
+++ edublocks-micropython/src/blocks/infrared/definitions.ts
@@ -1,13 +1,13 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
     Blocks['import_lirc'] = {
-        init: function() {
+        init() {
           this.appendDummyInput()
-              .appendField("import lirc");
+              .appendField('import lirc');
           this.setPreviousStatement(true, null);
           this.setNextStatement(true, null);
           this.setColour(20);
-       this.setTooltip("Import lirc");
-       this.setHelpUrl("");
+       this.setTooltip('Import lirc');
+       this.setHelpUrl('http://www.example.com/');
         }
       };
 }
