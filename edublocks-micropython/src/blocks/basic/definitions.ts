44ac638f7a4cd2c40665396210b1445b77fd34d4
best1029 <39964050+best1029@users.noreply.github.com>
2019-06-01 12:11:40 +0200

    Update definitions.ts

--- edublocks-micropython/src/blocks/basic/definitions.ts
+++ edublocks-micropython/src/blocks/basic/definitions.ts
@@ -1,17 +1,5 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
   
-   Blocks['import_lirc'] = {
-        init() {
-          this.appendDummyInput()
-              .appendField('import lirc');
-          this.setPreviousStatement(true, null);
-          this.setNextStatement(true, null);
-          this.setColour(20);
-       this.setTooltip('Import lirc');
-       this.setHelpUrl('http://www.example.com/');
-        }
-      };
-  
   Blocks['import_edupy'] = {
     init() {
       this.appendDummyInput()
