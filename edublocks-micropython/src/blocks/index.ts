7b8c4198a06cd791217e217b1203cbede9a3c17d
Stefan B <best1029@hs-karlsruhe.de>
2019-06-02 08:56:40 +0200

    infrared reaktiviert

--- edublocks-micropython/src/blocks/index.ts
+++ edublocks-micropython/src/blocks/index.ts
@@ -17,11 +17,11 @@ esp32Defs(Blockly.Blocks);
 esp32Gens(Blockly.Python as any);
 toolBoxXml += fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'esp32', 'toolbox.xml'));
 
-//import infraredDefs from './infrared/definitions';
-//import infraredGens from './infrared/generators';
-//infraredDefs(Blockly.Blocks);
-//infraredGens(Blockly.Python as any);
-//toolBoxXml += fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'infrared', 'toolbox.xml'));
+import infraredDefs from './infrared/definitions';
+import infraredGens from './infrared/generators';
+infraredDefs(Blockly.Blocks);
+infraredGens(Blockly.Python as any);
+toolBoxXml += fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'blocks', 'infrared', 'toolbox.xml'));
 
 import gpiozeroDefs from './gpiozero/definitions';
 import gpiozeroGens from './gpiozero/generators';
