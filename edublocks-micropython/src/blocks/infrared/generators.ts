1c39e98d796408171519dd26d817b7fa37e40d4d
Stefan B <best1029@hs-karlsruhe.de>
2019-06-05 11:15:02 +0200

    test ir

--- edublocks-micropython/src/blocks/infrared/generators.ts
+++ edublocks-micropython/src/blocks/infrared/generators.ts
@@ -1,6 +1,33 @@
 export default function define(Python: Blockly.BlockGenerators) {
     Python['import_lirc'] = function (block) {
-        const code = 'import lirc\n';
+        //const code = 'import lirc\n';
+        const code = 'import RPi.GPIO as GPIO \n' +
+        'import time \n'
+        
+        'sensor = 16 \n'
+        //'buzzer = 18 \n'
+        
+        'GPIO.setmode(GPIO.BOARD) \n'
+        'GPIO.setup(sensor,GPIO.IN) \n'
+        //'GPIO.setup(buzzer,GPIO.OUT) \n'
+        
+        //'GPIO.output(buzzer,False) \n'
+        'print "IR Sensor Ready....." \n'
+        'print " " \n'
+        
+        // 'try: \n'
+        //    'while True: \n'
+        //       'if GPIO.input(sensor): \n'
+        //           //'GPIO.output(buzzer,True) \n'
+        //           'print "Object Detected" \n'
+        //           'while GPIO.input(sensor): \n'
+        //               'time.sleep(0.2) \n'
+        //       'else: \n'
+        //           'GPIO.output(buzzer,False) \n'
+        
+        
+        // 'except KeyboardInterrupt: \n'
+        //     'GPIO.cleanup() \n'
         return code;
       };
 }
