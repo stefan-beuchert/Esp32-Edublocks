59226832ce66aa24b6a1df3126fd13abf34eeea2
Stefan B <best1029@hs-karlsruhe.de>
2019-06-20 15:29:11 +0200

    ir-remote-block-besser

--- edublocks-micropython/src/blocks/infrared/definitions.ts
+++ edublocks-micropython/src/blocks/infrared/definitions.ts
@@ -2,7 +2,9 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
   Blocks['using_ir_remote'] = {
       init() {
         this.appendDummyInput()
-            .appendField('using ir remote');
+            .appendField('using ir remote')
+            .appendField('PIN = ')
+            .appendField(new Blockly.FieldTextInput('pin'), 'pin')
         this.appendStatementInput('DO')
             .appendField('');
         this.setPreviousStatement(true, null);
--- edublocks-micropython/src/blocks/infrared/generators.ts
+++ edublocks-micropython/src/blocks/infrared/generators.ts
@@ -3,56 +3,57 @@ export default function define(Python: Blockly.BlockGenerators) {
         let branch = Blockly.Python.statementToCode(block, 'DO');
         branch = Blockly.Python.addLoopTrap(branch, block.id) || Blockly.Python.PASS;
         const _pin = block.getFieldValue('pin');
-        const code = 'class Remote: \n' +
-	'def __init__(self):\n' +
-    '# Initialize GPIO\n' +
-	'	self.recv = Pin(' + _pin + ', Pin.IN , Pin.PULL_UP)\n' +
-	'	self.recv.irq(trigger = Pin.IRQ_RISING|Pin.IRQ_FALLING , handler = self._handler)\n' +
-	'	\n' +
-	'	# Initialize Buffer , see ISR note !\n' +
-	'	self.buffer = [0 for x in range(100)]\n' +
-	'	self.bin = 0\n' +
-	'	self.length = 0\n' +
-	'	\n' +
-	'	# Initialize Timing Property\n' +
-	'	self.prev_irq = 0\n' +
-	'	\n' +
-	'def _handler(self , source):\n' +
-	'	self.time = ticks_us()\n' +
-	'	if self.prev_irq == 0:\n' +
-	'		self.prev_irq = self.time\n' +
-	'		self.length = 0\n' +
-	'		return\n' +
-	'	self.buffer[self.length] = ticks_diff(self.time , self.prev_irq)\n' +
-	'	self.prev_irq = self.time\n' +
-	'	self.length += 1\n' +
-	'	\n' +
-	'def _routine(self):\n' +
-    '		while True :\n' +
-	'		sleep_ms(200)\n' +
-	'		if ticks_diff(ticks_us(),self.prev_irq) > 200000 and self.length > 0 :\n' +
-	'			print("DECODED = [{},{}] ".format(self.decode()[1],self.length))\n' +
-    '\n' +
-	'			self.length = 0\n' +
-	'			self.prev_irq = 0\n' +
-	'			for x in range(len(self.buffer)):\n' +
-    '				self.buffer[x] = 0\n' +
-    '           ' + branch + '\n' +	    
-	'\n' +
-	'def decode(self):\n' +
-	'	self.bin = 0\n' +
-	'	m = 50000\n' +
-	'	for x in range(self.length):\n' +
-	'		m = min(self.buffer[x],m)\n' +
-	'	for x in range(0,self.length,2):\n' +
-	'		if self.buffer[x+1] > m*3 and self.buffer[x] > m*3:\n' +
-	'			continue\n' +
-	'		if self.buffer[x+1] > self.buffer[x]*3//2 :\n' +
-	'			self.bin += 2**(x//2)\n' +
-	'		else :\n' +
-	'			pass\n' +
-	'	return hex(self.bin) , bin(self.bin)\n' +
-	'	\n' +
+        const code = 
+    'class Remote: \n' +
+	'   def __init__(self):\n' +
+    '   # Initialize GPIO\n' +
+	'	    self.recv = Pin(' + _pin + ', Pin.IN , Pin.PULL_UP)\n' +
+	'	    self.recv.irq(trigger = Pin.IRQ_RISING|Pin.IRQ_FALLING , handler = self._handler)\n' +
+	'	    \n' +
+	'	    # Initialize Buffer , see ISR note !\n' +
+	'	    self.buffer = [0 for x in range(100)]\n' +
+	'	    self.bin = 0\n' +
+	'	    self.length = 0\n' +
+	'	    \n' +
+	'	    # Initialize Timing Property\n' +
+	'	    self.prev_irq = 0\n' +
+	'	    \n' +
+	'   def _handler(self , source):\n' +
+	'	    self.time = ticks_us()\n' +
+	'	    if self.prev_irq == 0:\n' +
+	'		    self.prev_irq = self.time\n' +
+	'		    self.length = 0\n' +
+	'		    return\n' +
+	'	    self.buffer[self.length] = ticks_diff(self.time , self.prev_irq)\n' +
+	'	    self.prev_irq = self.time\n' +
+	'	    self.length += 1\n' +
+	'	    \n' +
+	'   def _routine(self):\n' +
+    '		    while True :\n' +
+	'		    sleep_ms(200)\n' +
+	'		    if ticks_diff(ticks_us(),self.prev_irq) > 200000 and self.length > 0 :\n' +
+	'			    print("DECODED = [{},{}] ".format(self.decode()[1],self.length))\n' +
+    '               \n' +
+	'			    self.length = 0\n' +
+	'			    self.prev_irq = 0\n' +
+	'			    for x in range(len(self.buffer)):\n' +
+    '				    self.buffer[x] = 0\n' +
+    '               ' + branch + '\n' +	    
+	'               \n' +
+	'   def decode(self):\n' +
+	'	    self.bin = 0\n' +
+	'	    m = 50000\n' +
+	'	    for x in range(self.length):\n' +
+	'		    m = min(self.buffer[x],m)\n' +
+	'	    for x in range(0,self.length,2):\n' +
+	'		    if self.buffer[x+1] > m*3 and self.buffer[x] > m*3:\n' +
+	'			    continue\n' +
+	'		    if self.buffer[x+1] > self.buffer[x]*3//2 :\n' +
+	'			    self.bin += 2**(x//2)\n' +
+	'		    else :\n' +
+	'			    pass\n' +
+	'	    return hex(self.bin) , bin(self.bin)\n' +
+	'	    \n' +
     'remote = Remote()\n' +
     'remote._routine()';
 
