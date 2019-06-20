d6edbac4139f70688cb15a9c6f722eb4e3d9fad2
Stefan B <best1029@hs-karlsruhe.de>
2019-06-20 11:58:22 +0200

    lirc

--- edublocks-micropython/src/blocks/infrared/generators.ts
+++ edublocks-micropython/src/blocks/infrared/generators.ts
@@ -9,24 +9,7 @@ export default function define(Python: Blockly.BlockGenerators) {
         // 'print "IR Sensor Ready....." \n' +
         // 'print " "';
         
-        const code = 'import pulseio \n' +
-        'import board \n' +
-        'import adafruit_irremote \n' +
-        'pulsein = pulseio.PulseIn(board.REMOTEIN, maxlen=120, idle_state=True) \n' +
-        'decoder = adafruit_irremote.GenericDecode() \n' +
-
-        'while True: \n' +
-        '   pulses = decoder.read_pulses(pulsein) \n' +
-        '   print("Heard", len(pulses), "Pulses:", pulses) \n' +
-        '   try: \n' +
-        '       code = decoder.decode_bits(pulses, debug=False) \n' +
-        '       print("Decoded:", code) \n' +
-        '   except adafruit_irremote.IRNECRepeatException:  # unusual short code! \n' +
-        '       print("NEC repeat!") \n' +
-        '   except adafruit_irremote.IRDecodeException as e:     # failed to decode \n' +
-        '       print("Failed to decode: ", e.args) \n' +
-
-        '   print("----------------------------")';
+        const code = 'import lirc';
         return code;
     };
 }
